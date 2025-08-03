324361ef5bccf14a20fb1283aec6e14c0ab39c77
zixan-uz <zilolanorova7@gmail.com>
2025-08-03 18:10:27 +0500

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, left_sidebar, right_sidebar, no_sidebar
 
 urlpatterns = [
     path('', home, name='home'),
+    path('left-sidebar/', left_sidebar, name='left_sidebar'),
+    path('right-sidebar/', right_sidebar, name='right_sidebar'),
+    path('no-sidebar/', no_sidebar, name='no_sidebar'),
 ]
