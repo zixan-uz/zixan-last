8bb62663de93b388649d767f0af84d351ee2f6bd
zixan-uz <zilolanorova7@gmail.com>
2025-08-03 18:11:20 +0500

    Update views.py

--- main/views.py
+++ main/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base.html')
+
+def left_sidebar(request):
+    return render(request, 'left_sidebar.html')
+
+def right_sidebar(request):
+    return render(request, 'right_sidebar.html')
+
+def no_sidebar(request):
+    return render(request, 'no_sidebar.html')
