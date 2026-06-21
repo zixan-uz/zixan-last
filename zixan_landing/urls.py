d0061614a79f767c5058c598b20fbf02d883c789
zixan-uz <zilolanorova7@gmail.com>
2026-06-22 02:19:15 +0500

    Wire intake urls into root urlconf

--- zixan_landing/urls.py
+++ zixan_landing/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('diamor/', include('diamor_runtime.urls')),
+    path('intake/', include('intake.urls')),
     path('', include('main.urls')),
 ]
